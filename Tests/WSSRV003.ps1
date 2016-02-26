ba3e1b5aeed31c10d5337be8fab1303f09911912
damianscoles <damian@scolesfamily.net>
2016-02-26 09:15:03 -0600

    Edited test and added additional reporting variables.
    QnA on test as well as adding Info and Warning variables.

--- Tests/WSSRV003.ps1
+++ Tests/WSSRV003.ps1
@@ -1,6 +1,6 @@
 #requires -Modules ExchangeAnalyzer
 
-#This function checks the pagefile size and if it is managed
+#This function checks the pagefile is set to RAM + 10MB
 Function Run-WSSRV003()
 {
 
@@ -12,13 +12,15 @@ Function Run-WSSRV003()
 
     $PassedList = @()
     $FailedList = @()
+    $WarningList = @()
+    $InfoList = @()
     $ErrorList = @()
 
     foreach ($server in $exchangeservers) {
         $name = $server.name
 
         # Get RAM of remote server
-        $RAMinMB = (Get-WmiObject -Class win32_physicalmemory -computer $server | measure-object -property capacity -sum).sum/1048576
+        $RAMinMB = (Get-WmiObject -Class win32_physicalmemory -computer $name | measure-object -property capacity -sum).sum/1048576
         write-verbose "The $name server has $RAMinMB MB of RAM installed"
 
         # Get IP Address of remote server
@@ -30,46 +32,37 @@ Function Run-WSSRV003()
         $RAMIdeal = $RAMinMB+10
         $CurrentPageFile = (Get-WmiObject -computer $server Win32_PageFileUsage).allocatedbasesize
 
-        # Get Initial and Maximum PageFileSize
-        $is = wmic /node:"$ip" pagefileset get initialsize
-        $ms = wmic /node:"$ip" pagefileset get maximumsize
+        # Check if the pagefile is managed
+        if ($managed -ne $true) {
+        
+            # Get Initial and Maximum PageFileSize
+            $is = wmic /node:"$ip" pagefileset get initialsize
+            $ms = wmic /node:"$ip" pagefileset get maximumsize
 
-        # Split pagefile value to get number only
-        $PFInitSize = $is.Split([char]0x000D)
-        $PFMaxSize = $is.Split([char]0x000D)
+            # Split pagefile value to get number only
+            $PFInitSize = $is.Split([char]0x000D)
+            $PFMaxSize = $is.Split([char]0x000D)
 
-        # Select the numerical value from the PageFile variables
-        $pfis = $PFInitSize[2]
-        $pfms = $PFMAXSize[2]
+            # Select the numerical value from the PageFile variables
+            $pfis = $PFInitSize[2]
+            $pfms = $PFMAXSize[2]
 
-        # Remove extra spaces
-        $pfis = $pfis -replace '\s',''
-        $pfms = $pfms -replace '\s',''
+            # Remove extra spaces
+            $pfis = $pfis -replace '\s',''
+            $pfms = $pfms -replace '\s',''
 
-        # Check to see if PageFile is System Managed
-        if ($Managed -ne $true) {
-            write-verbose "The $name server does not have a system managed PageFile."
-            # Check if Initial and Maximum Pagefile are the same
-            if ($pfis -eq $pfms) {
-
-                # Check to make sure that the PageFile is the same size as RAM + 10Mb
-                if ($pfms -like $RAMIdeal) {
-                    $PassedList += $($name)
-                    write-verbose "The $name server has the correct PageFile size of $RAMIdeal MB."
-                } else {
-                    $FailedList += $($name)
-                    write-verbose "The $name server does not have the correct PageFile size of $RAMIdeal MB."
-                    write-host "The PageFile set to $CurrentPageFile MB."
-                }
+            # Check to make sure that the PageFile is the same size as RAM + 10Mb
+            if ($pfms -like $RAMIdeal) {
+                $PassedList += $($name)
+                write-verbose "The $name server has the correct PageFile size of $RAMIdeal MB."
             } else {
                 $FailedList += $($name)
-                write-verbose "The $name server does not have the initial and maximum PageFile set to the same value."
-                write-verbose "The initial PageFile is set to $pfis."
-                write-verbose "The maximum PageFile is set to $pfmx."
+                write-verbose "The $name server does not have the correct PageFile size of $RAMIdeal MB."
+                write-verbose "The PageFile set to $CurrentPageFile MB."
             }
         } else {
             $FailedList += $($name)
-            write-verbose "The $name server has a system managed PageFile."
+            write-verbose "The PageFile is System Managed and fails this test."
         }
     }
 
@@ -78,6 +71,8 @@ Function Run-WSSRV003()
                                       -TestId $TestID `
                                       -PassedList $PassedList `
                                       -FailedList $FailedList `
+                                      -WarningList $WarningList `
+                                      -InfoList $InfoList `
                                       -ErrorList $ErrorList `
                                       -Verbose:($PSBoundParameters['Verbose'] -eq $true)
 
