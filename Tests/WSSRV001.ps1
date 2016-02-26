9a6d59381612b95caca2ee88fc70b956da9af14d
damianscoles <damian@scolesfamily.net>
2016-02-26 09:10:46 -0600

    Added additional information for test WSSRV001
    Added the additional reporting variables - Info and Warning.

--- Tests/WSSRV001.ps1
+++ Tests/WSSRV001.ps1
@@ -1,6 +1,6 @@
 #requires -Modules ExchangeAnalyzer
 
-#This function checks to see if the C: Drive has greater than 30% free space
+#This function checks to see if the Exchange Install Drive has greater than 30% free space
 Function Run-WSSRV001()
 {
 
@@ -12,10 +12,32 @@ Function Run-WSSRV001()
 
     $PassedList = @()
     $FailedList = @()
+    $WarningList = @()
+    $InfoList = @()
     $ErrorList = @()
 
     foreach ($server in $exchangeservers) {
         $name = $server.name
+        if ($server.AdminDisplayVersion -match "Version 14") {$ver = "V14"}
+        if ($server.AdminDisplayVersion -match "Version 15") {$ver = "V15"}
+
+        Write-Verbose "Checking $name."
+
+        # Null out variables for clean results
+        $installpath = $null
+        $reg = $null
+
+        # Get the Exchange Install Drive
+        try {
+            $reg = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey("LocalMachine",$name)
+        } catch {
+            Write-Warning $_.Exception.Message
+            $installpath = "Unable to connect to registry"
+        }
+        if (!($installpath)) {
+            $installpath = $reg.OpenSubKey("SOFTWARE\Microsoft\ExchangeServer\$ver\Setup").GetValue("MsiInstallPath")
+        }
+        $ExchangeDrive = $InstallPath.substring(0,2)
 
         # Get free Space for all drives on the Exchange Server
         $space = Get-WMIObject win32_logicaldisk -computername $name
@@ -23,11 +45,11 @@ Function Run-WSSRV001()
         # Go through each drive on the server
         foreach ($line in $space) {
 
-            # Pull just the drive letter
+            # Get the current drive in the loop
             $drive = $line.DeviceID
 
-            # Look just for the C: drive for percent free space
-            if ($drive -eq "C:") {
+            # Look just for the Exchange Install Drive for percent free space
+            if ($drive -eq $ExchangeDrive) {
                 $free = $line.freespace
                 $size = $line.size
 
@@ -37,10 +59,17 @@ Function Run-WSSRV001()
                 # Round the percentage
                 $percent = [math]::Round($percent2,2)
 
-                # Check to se if C: drive has 30% or greater free space
-                if ($percent -lt "30") {
+                # Check to see if the Exchange Install Drive has 30% or greater free space
+                if ($percent -lt "15") {
+                    write-verbose "The Exchange Install Drive has less than 30% free space."
                     $FailedList += $($name)
-                } else {
+                } 
+                if (($percent -gt "15") -and ($percent -lt "30")) {
+                   write-verbose "The Exchange Install Drive has greater than 15% and less than 30% free space."
+                    $WarningList += $($name)
+                }
+                if ($percent -gt "30") {
+                    write-verbose "The Exchange Install Drive has greater than 30% free space."
                     $PassedList += $($name)
                 }
             }
@@ -52,6 +81,8 @@ Function Run-WSSRV001()
                                       -TestId $TestID `
                                       -PassedList $PassedList `
                                       -FailedList $FailedList `
+                                      -WarningList $WarningList `
+                                      -InfoList $InfoList `
                                       -ErrorList $ErrorList `
                                       -Verbose:($PSBoundParameters['Verbose'] -eq $true)
 
