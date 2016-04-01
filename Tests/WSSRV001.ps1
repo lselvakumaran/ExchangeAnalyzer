4bcb397c4d1c66b957195fede02aaf5c53e2d1e5
Brian Desmond <brian@briandesmond.com>
2016-04-01 18:38:22 -0500

    - Created helper methods to publish a server property bag for consumption by tests - Created stub helpers for Registry and WMI. These need to have work done on them to add error handling - Refactored WSSRV01 to use these additions. - Removed HW001 to clean up the branch

--- Tests/WSSRV001.ps1
+++ Tests/WSSRV001.ps1
@@ -7,7 +7,7 @@ Function Run-WSSRV001()
    [CmdletBinding()]
     param()
 
-    $TestID = "WSSRV001"
+    $TestID = 'WSSRV001'
     Write-Verbose "----- Starting test $TestID"
 
     $PassedList = @()
@@ -16,70 +16,56 @@ Function Run-WSSRV001()
     $InfoList = @()
     $ErrorList = @()
 
-    foreach ($server in $exchangeservers) {
-        $ServerName = $server.name
-        if ($server.AdminDisplayVersion -match "Version 14") {$ver = "V14"}
-        if ($server.AdminDisplayVersion -match "Version 15") {$ver = "V15"}
+    foreach ($server in $ExchangeServers) 
+    {
+        $serverName = $server.name
 
-        Write-Verbose "Checking $ServerName."
+        Write-Verbose "Checking $serverName"
 
         # Null out variables for clean results
-        $installpath = $null
         $reg = $null
 
-        # Get the Exchange Install Drive
-        try
+		$reg = Get-ExAServerProperty -Server $serverName -Property 'ExchangeInstallPath'
+
+        if ($reg -eq $null)
         {
-            $reg = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey("LocalMachine",$ServerName)
-        }
-        catch
-        {
-            Write-Warning $_.Exception.Message
-            $installpath = "Unable to connect to registry"
+            Write-Verbose "ExchangeInstallPath property null for $serverName"
+            $FailedList += @($serverName)
+            continue
         }
 
-        if (!($installpath))
-        {
-            $installpath = $reg.OpenSubKey("SOFTWARE\Microsoft\ExchangeServer\$ver\Setup").GetValue("MsiInstallPath")
-        }
-        $ExchangeInstallDrive = $InstallPath.substring(0,2)
+        [string]$exchangeInstallDrive = [IO.Path]::GetPathRoot($reg)
+        # trim the trailing \
+        $exchangeInstallDrive = $exchangeInstallDrive.Substring(0, 2)
 
         # Get free Space for all drives on the Exchange Server
-        $LogicalDisks = Get-WMIObject win32_logicaldisk -computername $ServerName
+        ##TODO: Consider publishing this to the property bag if we need to test multiple things about this disk
+        $logicalDisk = Get-ExAWmiObject -Computer $serverName -Class 'Win32_LogicalDisk' -Property @('DeviceID', 'FreeSpace', 'Size') -Filter "DeviceID='$ExchangeInstallDrive'"
     
-        # Go through each drive on the server
-        foreach ($LogicalDisk in $LogicalDisks)
+        $free = $LogicalDisk.FreeSpace
+        $size = $LogicalDisk.Size
+
+        Write-Verbose "Server $serverName Disk $exchangeInstallDrive is $size with $free free space"
+
+        # Calculate percent free space
+        [int]$percentFree = ($free / $size) * 100
+
+        
+        if ($percentFree -lt 15) 
         {
-
-            # Get the current drive in the loop
-            $drive = $LogicalDisk.DeviceID
-
-            # Look just for the Exchange Install Drive for percent free space
-            if ($drive -eq $ExchangeInstallDrive) {
-                $free = $LogicalDisk.freespace
-                $size = $LogicalDisk.size
-
-                # Calculate percent free space
-                $percent2 = ($free/$size)*100
-
-                # Round the percentage
-                $percent = [math]::Round($percent2,2)
-
-                # Check to see if the Exchange Install Drive has 30% or greater free space
-                if ($percent -lt "15") {
-                    write-verbose "The Exchange Install Drive has less than 30% free space."
-                    $FailedList += $($ServerName)
-                } 
-                if (($percent -gt "15") -and ($percent -lt "30")) {
-                   write-verbose "The Exchange Install Drive has greater than 15% and less than 30% free space."
-                    $WarningList += $($ServerName)
-                }
-                if ($percent -gt "30") {
-                    write-verbose "The Exchange Install Drive has greater than 30% free space."
-                    $PassedList += $($ServerName)
-                }
-            }
+            Write-Verbose "Install Drive on $serverName has less than 15% free space."
+            $FailedList += $($ServerName)
+        } 
+        elseif (($percentFree -gt 15) -and ($percentFree -lt 30)) 
+        {
+            Write-Verbose "Install Drive on $serverName has 15% - 30% free space."
+            $WarningList += $($ServerName)
         }
+        else
+        {
+            Write-Verbose "Install Drive on $serverName has more than 30% free space."
+            $PassedList += $($ServerName)
+        }            
     }
 
     #Roll the object to be returned to the results
